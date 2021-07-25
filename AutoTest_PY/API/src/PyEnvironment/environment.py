f423a0072c1297925f01e6d4edcd8a11d8b05bfc
hwx927099 <hsc123456>
2021-07-25 20:23:09 +0800

    提交

---
+++ AutoTest_PY/API/src/PyEnvironment/environment.py
@@ -0,0 +1,23 @@
+import os
+from pip._internal import main
+
+
+"""
+执行如下命令，生成pip.ini,将pipi下载路径设置为豆瓣
+"""
+ini = "[global]\nindex-url = https://pypi.doubanio.com/simple/\n"
+pippath=os.environ["USERPROFILE"]+"\\pip\\"
+exec("if not os.path.exists(pippath):\n\tos.mkdir(pippath)")
+open(pippath+"/pip.ini","w+").write(ini)
+
+
+"""
+安装requirements.txt内的第三方包
+"""
+main(['install', '-r', 'requirements.txt'])
+
+# """
+# 生成requirements.txt文件
+# """
+# main(['freeze', '>', 'requirements.txt'])
+
