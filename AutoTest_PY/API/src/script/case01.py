4fbc68f85fa03eae5b4fb5ab54336c0709068279
hwx927099 <hsc123456>
2021-07-28 00:12:13 +0800

    提交

--- AutoTest_PY/API/src/script/case01.py
+++ AutoTest_PY/API/src/script/case01.py
@@ -1,16 +1,38 @@
 import unittest
 
-from src.Common.Log import  Logger
+import urllib3
+
+from src.Api.ecshop import Login
+from src.Common.Http import Http
+from src.Common.Log import Logger
+from src.Common.util import util
 
 
-class Case01(unittest.TestCase):
-    logger=Logger().get_log().logger
-    url = 'https://api.apiopen.top/getJoke?page=1&count=2&type=video'
+class Case01(unittest.TestCase, Http):
 
+    logger = Logger().get_log().logger
+    util = util()
+    login = Login.Login()
+    data = {"username": "hsc", "password": "hsc123456", "act": "act_login", "back_act": "user.php", "submit": ""}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
     def setUp(self):
-        self.logger.info("开始执行用例")
+        urllib3.disable_warnings()
+        self.logger.info(50*"="+"开始执行用例"+50*"=")
+
     def tearDown(self):
-        self.logger.info("用例执行结束")
+        self.logger.info(50*"="+"用例执行结束"+50*"=")
+
     def test_getSupportCity(self):
-        self.logger.info("用例步骤执行")
+        self.logger.info(50*"="+"用例步骤执行"+50*"=")
+        try:
+            response = self.login.login_ecshop(self.data, self.headers)
+            self.assertEqual(response.status_code, 200, msg="1.1 验证返回码为200")
+            self.assertEqual("欢迎光临本店" in response.text, True, msg="1.2 验证返回信息正确")
+            self.assertEqual( response.elapsed.total_seconds() < 3,True, msg="1.3 响应时间小于3秒")
+        except AssertionError as e:
+            self.logger.info(e)
+            pass
+
+
+
--- AutoTest_PY/API/src/script/case02.py
+++ AutoTest_PY/API/src/script/case02.py
@@ -1,16 +1,31 @@
 import unittest
+
+import urllib3
+
+from src.Api.ecshop.Message import Message
 from src.Common.Log import Logger
 
 
 class Case01(unittest.TestCase):
     logger = Logger().get_log().logger
-    url = 'https://api.apiopen.top/getJoke?page=1&count=2&type=video'
+    message = Message()
+    data = {"user_email": "2427697226@qq.con", "msg_type": "0", "msg_title": "留言标题", "msg_content": "留言内容",
+            "act": "act_add_message"}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
     def setUp(self):
-        self.logger.info("开始执行用例")
+        urllib3.disable_warnings()
+        self.logger.info(50*"="+"开始执行用例"+50*"=")
 
     def tearDown(self):
-        self.logger.info("用例执行结束")
+        self.logger.info(50*"="+"用例执行结束"+50*"=")
 
     def test_getSupportCity(self):
-        self.logger.info("用例步骤执行")
+        self.logger.info(50*"="+"用例步骤执行"+50*"=")
+        response = self.message.post_message(self.data, self.headers)
+        try:
+            self.assertEqual(response.status_code, 200, msg="验证返回码为200")
+            self.assertEqual("您至少在30秒后才可以继续发表评论!" in response.text, True, msg="验证返回码为200")
+            self.assertEqual( response.elapsed.total_seconds() < 3,True, msg="验证返回码为200")
+        except AssertionError as e:
+            pass  # 抛出异常继续往下执行
