6895571b8fa5e6d35970ff22d306b0e81642c79c
hwx927099 <hsc123456>
2021-07-31 18:23:56 +0800

    提交

--- AutoTest_PY/API/src/Common/Http.py
+++ AutoTest_PY/API/src/Common/Http.py
@@ -1,7 +1,8 @@
 import requests
 import urllib3
 from src.Common.Log import Logger
-from src.Common.util import util
+from util import util
+
 
 class Http():
     urllib3.disable_warnings()
@@ -9,53 +10,36 @@ class Http():
 
     def __init__(self):
         global basicPath, hostIp, timeout
-        basicPath = self.util.get_http("basicPath")
-        hostIp = self.util.get_http("hostIp")
         timeout = self.util.get_http("timeout")
         self.logger = Logger().get_log().logger
-        self.headers = {}
-        self.params = {}
-        self.data = {}
-        self.url = None
-        self.files = {}
-        self.json = {}
-
-    def set_url(self, url):
-        self.url = basicPath + hostIp + url
-        self.logger.info("url : "+self.url)
-
-    def set_headers(self, headers):
-        self.headers = headers
-        self.logger.info("headers : "+str(self.headers))
-
-    def set_params(self, param):
-        self.params = param
-
-    def set_data(self, data):
-        self.data = data
-        self.logger.info("data : "+str(self.data))
-
-    def set_json(self, json):
-        self.json = json
-
-    def set_files(self, file):
-        self.files = file
 
     # get请求方法
-    def get(self):
+    def get(self, url, headers, cookies, *args):
         try:
-            response = requests.get(self.url, params=self.params, headers=self.headers, timeout=float(timeout))
+            self.logger.info("url: " + url)
+            self.logger.info("headers: " + str(headers))
+            self.logger.info("cookies: " + str(cookies))
+            self.logger.info("params: " + str(args))
+            response = requests.get(url, headers=headers, cookies=cookies, timeout=float(timeout), *args)
+            self.logger.info("code: " + str(response.status_code))
+            self.logger.info("response: " + response.text)
+
             return response
         except TimeoutError:
             self.logger.error("Time out")
             return None
 
     # post请求方法
-    def post(self):
-        self.logger.info("method : "+"post")
+    def post(self, url, headers, cookies, *args):
+        self.logger.info("requestMethod : " + "post")
         try:
-            response = requests.post(self.url, data=self.data, json=self.json, headers=self.headers, files=self.files,verify=False)
-            # self.logger.info("response : " + response.text)
+            self.logger.info("url: " + url)
+            self.logger.info("headers: " + str(headers))
+            self.logger.info("cookies: " + str(cookies))
+            self.logger.info("params: " + str(args))
+            response = requests.post(url, headers=headers, cookies=cookies, verify=False, *args)
+            self.logger.info("code: " + str(response.status_code))
+            self.logger.info("response: " + response.text)
             return response
         except TimeoutError:
             self.logger.error("Time out")
@@ -63,18 +47,30 @@ class Http():
             return None
 
     # delete请求方法
-    def delete(self):
+    def delete(self, url, headers, cookies, *args):
         try:
-            response = requests.delete(self.url, params=self.params, headers=self.headers, timeout=float(timeout))
+            self.logger.info("url: " + url)
+            self.logger.info("headers: " + str(headers))
+            self.logger.info("cookies: " + str(cookies))
+            self.logger.info("params: " + str(args))
+            response = requests.delete(url, headers=headers, cookies=cookies, timeout=float(timeout))
+            self.logger.info("code: " + str(response.status_code))
+            self.logger.info("response: " + response.text)
             return response
         except TimeoutError:
             self.logger.error("Time out")
             return None
 
     # put请求方法
-    def put(self):
+    def put(self, url, headers, cookies, *args):
         try:
-            response = requests.put(self.url, data=self.data, headers=self.headers, timeout=float(timeout))
+            self.logger.info("url: " + url)
+            self.logger.info("headers: " + str(headers))
+            self.logger.info("cookies: " + str(cookies))
+            self.logger.info("params: " + str(args))
+            response = requests.put(url, headers=headers, cookies=cookies, timeout=float(timeout), *args)
+            self.logger.info("code: " + str(response.status_code))
+            self.logger.info("response: " + response.text)
             return response
         except TimeoutError:
             self.logger.error("Time out")
