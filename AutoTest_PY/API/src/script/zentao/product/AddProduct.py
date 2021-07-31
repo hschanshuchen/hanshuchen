9517849ab6d2cbaa1007bd46d69b2840970be527
hwx927099 <hsc123456>
2021-07-31 18:34:13 +0800

    提交

--- AutoTest_PY/API/src/script/zentao/product/AddProduct.py
+++ AutoTest_PY/API/src/script/zentao/product/AddProduct.py
@@ -1,5 +1,7 @@
 import re
 import unittest
+
+from Config import Config
 from product.Product import Product
 from src.Common.Log import Logger
 from util import util
@@ -8,14 +10,15 @@ from util import util
 class AddProduct(unittest.TestCase):
     def setUp(self):
         self.logger = Logger().get_log().logger
-        self.logger.info("开始执行用例")
-        self.hostIp = util().get_http("localhost")
-        self.username = util().get_user("username")
+        self.logger.info("开始执行用例 %s " % self.id())
+        self.conf = Config()
+        self.hostIp = self.conf.get_http("localhost")
+        self.username = self.conf.get_user("username")
         self.product = Product()
         self.productIdList = []
 
     def test_addProduct_001(self):
-        self.logger.info("用例步骤执行")
+        self.logger.info("用例步骤执行 %s" % self.id())
         self.data = {"name": "productName_" + util().getRandomStr(5),
                      "code": "productCode_" + util().getRandomStr(5),
                      "line": 0,
@@ -33,27 +36,8 @@ class AddProduct(unittest.TestCase):
         self.assertEqual("保存成功", msg, "返回提示信息保存成功")
         self.productIdList.append(int(re.findall(r"product-browse-(.+?)\.", response.text)[0]))
 
-    def test_addProduct_001(self):
-        self.logger.info("用例步骤执行")
-        self.data = {"name": "productName_" + util().getRandomStr(5),
-                     "code": "productCode_" + util().getRandomStr(5),
-                     "line": 0,
-                     "PO": self.username,
-                     "QD": "",
-                     "RD": "",
-                     "type": "normal",
-                     "desc": "",
-                     "acl": "open",
-                     "uid": "6104402102131"
-                     }
-        response = self.product.addProduct(self.hostIp, self.data)
-        self.assertEqual(200, response.status_code, "验证码是200")
-        msg = re.findall(r"alert\('(.+?)'\)", response.text)[0]
-        self.assertEqual("保存成功", msg, "返回提示信息保存成功")
-        self.productId = int(re.findall(r"product-browse-(.+?)\.", response.text)[0])
-
     def test_addProduct_002(self):
-        self.logger.info("用例步骤执行")
+        self.logger.info("执行用例步骤 %s " % self.id())
         productName = "productName_" + util().getRandomStr(5)
         self.data = {"name": productName,
                      "code": "productCode_" + util().getRandomStr(5),
@@ -68,14 +52,14 @@ class AddProduct(unittest.TestCase):
                      }
         response = self.product.addProduct(self.hostIp, self.data)
         self.assertEqual(200, response.status_code, "验证码是200")
-        self.productId = int(re.findall(r"product-browse-(.+?)\.", response.text)[0])
+        self.productIdList.append(int(re.findall(r"product-browse-(.+?)\.", response.text)[0]))
         response = self.product.addProduct(self.hostIp, self.data)
         msg = re.findall(r"alert\('(.+?)『productName", response.text)[0]
         self.assertEqual("『产品名称』已经有", msg, "产品名称重复校验")
 
     def test_addProduct_003(self):
-        self.logger.info("用例步骤执行")
-        self.data = {"name": util().getRandomStr(62)+util().getRandomStr(29),
+        self.logger.info("执行用例步骤 %s " % self.id())
+        self.data = {"name": util().getRandomStr(62) + util().getRandomStr(29),
                      "code": "productCode_" + util().getRandomStr(5),
                      "line": 0,
                      "PO": self.username,
@@ -92,7 +76,8 @@ class AddProduct(unittest.TestCase):
         self.assertEqual("『产品名称』长度应当不超过『90』，且大于『0』。", msg, "名称长度校验")
 
     def tearDown(self):
-        self.logger.info("用例执行结束")
+        self.logger.info("环境清理 %s " % self.id())
         for n in self.productIdList:
             response = self.product.deleteProduct(self.hostIp, n)
             self.assertEqual(200, response.status_code, "验证码是200")
+        self.logger.info("\n\n" + "=" * 180 + "\n")
