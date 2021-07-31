bde67f081a8fc939956549d0b877d37de505a11e
hwx927099 <hsc123456>
2021-07-31 18:29:42 +0800

    提交

---
+++ AutoTest_PY/API/src/Common/Config.py
@@ -0,0 +1,32 @@
+import configparser
+import os
+
+
+class Config:
+    def __init__(self):
+        self.configPath = os.path.abspath("../../.././config.ini")
+        self.conf = configparser.ConfigParser()
+        self.conf.read(self.configPath, encoding='utf-8')
+
+    def get_user(self, key):
+        """
+        读取配置文件用户用户信息
+        :param key:
+        :return:
+        """
+        return self.conf.get("USER", key)
+
+    def get_db(self, key):
+        """
+        读取配置文件数据库信息
+        :param key:
+        :return:
+        """
+        value = self.conf.get("DATABASE", key)
+        return value
+
+    def get_email(self, key):
+        return self.conf.get("EMAIL", key)
+
+    def get_http(self, key):
+        return self.conf.get("HTTP", key)
