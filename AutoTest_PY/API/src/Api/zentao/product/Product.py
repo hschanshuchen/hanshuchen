d7cd4ff380bf5879ec439af2940c6d62201def24
hwx927099 <hsc123456>
2021-07-31 18:23:12 +0800

    提交

--- AutoTest_PY/API/src/Api/zentao/product/Product.py
+++ AutoTest_PY/API/src/Api/zentao/product/Product.py
@@ -1,5 +1,7 @@
 import re
 import requests
+
+from Http import Http
 from login.Login import Login
 from util import util
 
@@ -7,22 +9,23 @@ from util import util
 class Product:
 
     def __init__(self):
+        self.http = Http()
         self.login = Login()
+        self.params = {}
+        self.files = {}
+        self.json = {}
         self.cookies = self.login.login_zentao()
         self.headers = {"Content-Type": "application/x-www-form-urlencoded",
                         "Authorization": "Basic emVudGFvOm4zcGEwSmpsak0x",
                         }
 
     def addProduct(self, hostIp, data):
-        respons = requests.post(url="http://%s/zentao/product-create.html" % hostIp, headers=self.headers, data=data,
-                                cookies=self.cookies)
+        respons = self.http.post("http://%s/zentao/product-create.html" % hostIp, self.headers, self.cookies, data)
         return respons
 
     def deleteProduct(self, hostIp, id):
-        cookies01 = self.cookies
-        respons = requests.post(url="http://%s/zentao/product-delete-%s-yes.html" % (hostIp, id),
-                                headers=self.headers,
-                                cookies=cookies01)
+        respons = self.http.post("http://%s/zentao/product-delete-%s-yes.html" % (hostIp, id), self.headers,
+                                 self.cookies)
         return respons
 
 
@@ -30,15 +33,24 @@ if __name__ == "__main__":
     p = Product()
     u = util()
 
-    # res = p.addProduct("127.0.0.1", u.getRandomStr(45), u.getRandomStr(45))
-    # print(res.text)
-    # id = int(res.text.split("self.location='/zentao/product-browse-")[1].split(".")[0])
-    # print(id)
+    data = {"name": "productName_" + util().getRandomStr(5),
+            "code": "productCode_" + util().getRandomStr(5),
+            "line": 0,
+            "PO": "admin",
+            "QD": "",
+            "RD": "",
+            "type": "normal",
+            "desc": "",
+            "acl": "open",
+            "uid": "6104402102131"
+            }
+    res = p.addProduct("127.0.0.1", data)
+    print(res.text)
 
     # for i in range(2000):
-    #     re=p.addProduct("127.0.0.1",u.getRandomStr(45),u.getRandomStr(45))
+    #     re = p.addProduct("127.0.0.1", u.getRandomStr(45), u.getRandomStr(45))
     #     print(re.text)
 
-    for i in range(20000):
-        re = p.deleteProduct("127.0.0.1", i)
-        print("第  %s  次执行删除"%i)
+    # for i in range(20000):
+    #     re = p.deleteProduct("127.0.0.1", i)
+    #     print("第  %s  次执行删除" % i)
