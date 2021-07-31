93856c4661bdc2d33471ef52d2923c57fbfd0b3f
hwx927099 <hsc123456>
2021-08-01 00:36:00 +0800

    提交

--- AutoTest_PY/API/src/Api/zentao/product/Product.py
+++ AutoTest_PY/API/src/Api/zentao/product/Product.py
@@ -1,5 +1,4 @@
-import re
-import requests
+from bs4 import BeautifulSoup
 
 from Http import Http
 from login.Login import Login
@@ -23,29 +22,40 @@ class Product:
         respons = self.http.post("http://%s/zentao/product-create.html" % hostIp, self.headers, self.cookies, data)
         return respons
 
+    def getAllProduct(self, hostIp):
+        respons = self.http.post("http://%s/zentao/product-all.html" % hostIp, self.headers, self.cookies)
+        return respons
+
     def deleteProduct(self, hostIp, id):
         respons = self.http.post("http://%s/zentao/product-delete-%s-yes.html" % (hostIp, id), self.headers,
                                  self.cookies)
         return respons
 
 
-if __name__ == "__main__":
-    p = Product()
-    u = util()
-
-    data = {"name": "productName_" + util().getRandomStr(5),
-            "code": "productCode_" + util().getRandomStr(5),
-            "line": 0,
-            "PO": "admin",
-            "QD": "",
-            "RD": "",
-            "type": "normal",
-            "desc": "",
-            "acl": "open",
-            "uid": "6104402102131"
-            }
-    res = p.addProduct("127.0.0.1", data)
-    print(res.text)
+# if __name__ == "__main__":
+#     p = Product()
+#     u = util()
+#     productIdList = []
+#     res = p.getAllProduct("127.0.0.1")
+#     soup = BeautifulSoup(res.content, "html.parser")
+#     productTagList = soup.find_all("input", type='checkbox')
+#
+#     for tag in productTagList:
+#         productIdList.append(tag.get("value"))
+#     print(productIdList)
+    # data = {"name": "productName_" + util().getRandomStr(5),
+    #         "code": "productCode_" + util().getRandomStr(5),
+    #         "line": 0,
+    #         "PO": "admin",
+    #         "QD": "",
+    #         "RD": "",
+    #         "type": "normal",
+    #         "desc": "",
+    #         "acl": "open",
+    #         "uid": "6104402102131"
+    #         }
+    # res = p.addProduct("127.0.0.1", data)
+    # print(res.text)
 
     # for i in range(2000):
     #     re = p.addProduct("127.0.0.1", u.getRandomStr(45), u.getRandomStr(45))
