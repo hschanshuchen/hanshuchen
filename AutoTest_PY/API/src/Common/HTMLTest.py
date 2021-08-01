0f93dbbb34ebdbe668b9c5bccc22dcd166458057
hwx927099 <hsc123456>
2021-08-01 23:05:26 +0800

    提交

--- AutoTest_PY/API/src/Common/HTMLTest.py
+++ AutoTest_PY/API/src/Common/HTMLTest.py
@@ -7,16 +7,16 @@ import os
 class HTML:
     def __init__(self):
         # 定义需要执行哪个包下的文件，我要运行项目的文件夹是：当前目录下的父级目录下的“testcases”目录
-        self.dir_test = os.path.abspath("../script/zentao/product")
-        print(self.dir_test)
+        self.dir_test = os.path.abspath("../script/")
         # 获取当前时间
         self.now = strftime("%Y_%m_%d-%H-%M-%S", localtime(time()))
         # 报告位置
         self.filename = os.path.abspath("..\\result\\report") + "\\" + self.now + "report.html"
+        print(self.filename)
 
     def htmlTest(self, title, description):
         # 使用unittest.TestLoader().discover方法，运行以case.py结尾的所有py文件
-        my_discover = unittest.TestLoader().discover(self.dir_test, "AddProduct.py")
+        my_discover = unittest.TestLoader().discover(self.dir_test, "*.py")
 
         with open(self.filename, "wb") as fp:
             # 定义报告标题，描述，以及报告的详细情况
